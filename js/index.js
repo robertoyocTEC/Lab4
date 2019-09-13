bc7852fd8c92c50bd1fd7c4f6ff5d47fb763206a
Roberto Yoc <roberto.yoc@energyza.energy>
2019-09-12 23:33:09 -0500

    clear funcionality

--- js/index.js
+++ js/index.js
@@ -4,11 +4,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
         event.preventDefault();
         const value = event.target.elements[1].value;
         if (value) {
-            const list =  document.getElementById('listTodo').innerHTML += `<div><input type="checkbox">${value}</div>`;
+            const list =  document.getElementById('listTodo').innerHTML += `<div><input class="todo-element" type="checkbox">${value}</div>`;
             event.target.elements[1].value = null
         }
     }
     document.getElementById('clearAll').onclick = function (event) {
-        
+        const list = document.getElementsByClassName('todo-element');
+        Array.from(list).forEach((element)=>{
+            element.checked = false;
+        })
+
     }
 });
